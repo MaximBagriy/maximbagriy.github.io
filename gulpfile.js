5af2a8068c9283ff7cb511da63f5da85eea99768
kookeron666 <maximbagrii@yandex.ru>
2020-07-06 19:38:17 +0300

    added header

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,7 @@ let gulp = require('gulp'),
     
                                             // Подключили SASS SCSS
 gulp.task('sass', function(){
-    return gulp.src('app/scss/style.scss')
+    return gulp.src('app/scss/**/*.scss')
         .pipe(sass({outputStyle: 'compressed'}))
         .pipe(rename({suffix: '.min'}))
         .pipe(autoprefixer({
@@ -24,8 +24,9 @@ gulp.task('sass', function(){
 gulp.task('style', function(){
     return gulp.src([
               //  Подключаем библиотеки
+        'node_modules/normalize.css/normalize.css',
         'node_modules/slick-carousel/slick/slick.css',                 
-        'node_modules/magnific-popup/dist/jquery.magnific-popup.css'
+        'node_modules/magnific-popup/dist/magnific-popup.css'
     ])
         .pipe(concat('libs.min.css'))
         .pipe(cssmin())
@@ -64,11 +65,11 @@ gulp.task('browser-sync', function() {
 });
                                                 //  Наблюдение за изменениями в css html js
 gulp.task('watch', function(){
-    gulp.watch('app/scss/style.scss', gulp.parallel('sass'))
+    gulp.watch('app/scss/**/*.scss', gulp.parallel('sass'))
     gulp.watch('app/*.html', gulp.parallel('html'))
     gulp.watch('app/js/*.js', gulp.parallel('js'))
 });
 
 
                                                 // Default'ное включение модулей
-gulp.task('default', gulp.parallel('style','script', 'sass', 'watch', 'browser-sync'))
+gulp.task('default', gulp.parallel('style', 'script', 'sass', 'watch', 'browser-sync'));
