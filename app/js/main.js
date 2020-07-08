a41ccabbaf6e0e38f67eafe6ec1d38e48fec8ee0
kookeron666 <maximbagrii@yandex.ru>
2020-07-09 00:15:24 +0300

    added adventages

--- app/js/main.js
+++ app/js/main.js
@@ -2,7 +2,21 @@ $(function(){
 
 // ВСТАВИТЬ В ИСХОДНИК!!!!!!!!!!!
 
-var mixer = mixitup('.products__inner-box');
 
+
+    var mixer = mixitup('.products__inner-box');
+
+  $(".rate-star").rateYo({
+      rating: 5,
+      starWidth: "12px",
+      readOnly: true
+    });
+
+    $(".product-slider__inner").slick({
+        dots:true,
+        arrows: false,
+        slidesToShow: 4,
+        slidesToScroll: 4
+    });
 
 });
