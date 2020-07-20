152d5b4ad8cfb57c74249bc0600c184b66dc95fc
kookeron666 <maximbagrii@yandex.ru>
2020-07-20 22:29:54 +0300

    added new-items palge

--- app/js/main.js
+++ app/js/main.js
@@ -2,17 +2,11 @@ $(function(){
 
 // ВСТАВИТЬ В ИСХОДНИК!!!!!!!!!!!
 
-
-
-    var mixer = mixitup('.products__inner-box');
-
-  $(".rate-star").rateYo({
+    $(".rate-star").rateYo({
       rating: 5,
       starWidth: "12px",
       readOnly: true
     });
-
-    
 
     $(".product-slider__inner").slick({
         dots:true,
@@ -20,5 +14,29 @@ $(function(){
         slidesToShow: 4,
         slidesToScroll: 4
     });
+
+    $(".js-range-slider").ionRangeSlider({
+        type: "double",
+        min: 0,
+        max: 1000,
+        from: 0,
+        to: 600,
+        prefix: "$"
+    });
+
+    $('.icon-th-list').on('click', function(){
+        $('.product__item').addClass('list');
+        $('.icon-th-list').addClass('active');
+        $('.icon-th-large').removeClass('active');
+    });
+    $('.icon-th-large').on('click', function(){
+        $('.product__item').removeClass('list'); 
+        $('.icon-th-list').removeClass('active');
+        $('.icon-th-large').addClass('active');
+
+    });
+
+    var mixer = mixitup('.products__inner-box');
+    
 
 });
